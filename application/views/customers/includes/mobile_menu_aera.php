a681c7e1a522ebef2ff79fff698fb2564d92b689
snehalk2210 <55176210+snehalk2210@users.noreply.github.com>
2019-09-15 11:47:13 +0530

    Add files via upload
    changes

--- application/views/customers/includes/mobile_menu_aera.php
+++ application/views/customers/includes/mobile_menu_aera.php
@@ -15,7 +15,10 @@
             <div class="header-top-area">
                 <div class="container-fluid">
                     <div class="row">
-                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                    	
+                    </div>
+                        <div class="col-lg-6 col-md-6col-sm-6 col-xs-12">
                             <div class="header-top-wraper">
                                 <div class="row">
                                     <div class="col-lg-1 col-md-0 col-sm-1 col-xs-12">
@@ -175,19 +178,19 @@
                                                 <li class="nav-item">
                                                     <a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle">
 														
-															<span class="admin-name" ><?php $user=$this->session->userdata('user_name');echo $user;?></span>
+															<span class="admin-name" ><?php $user=$this->session->userdata('usr_name');echo $user;?></span>
 															<i class="fa fa-angle-down edu-icon edu-down-arrow"></i>
 														</a>
                                                     <ul role="menu" class="dropdown-header-top author-log dropdown-menu animated zoomIn">
                                                         <li><a href="#"><span class="edu-icon edu-home-admin author-log-ic"></span>My Account</a>
                                                         </li>
-                                                        <li><a href="#"><span class="edu-icon edu-user-rounded author-log-ic"></span><?php $user=$this->session->userdata('user_name');echo $user;?></a>
+                                                        <li><a href="#"><span class="edu-icon edu-user-rounded author-log-ic"></span><?php $user=$this->session->userdata('usr_name');echo $user;?></a>
                                                         </li>
                                                         <li><a href="#"><span class="edu-icon edu-money author-log-ic"></span>User Billing</a>
                                                         </li>
                                                         <li><a href="#"><span class="edu-icon edu-settings author-log-ic"></span>Settings</a>
                                                         </li>
-                                                        <li><a href="<?php base_url().''?>"><span class="edu-icon edu-locked author-log-ic"></span>Log Out</a>
+                                                        <li><a href="<?php base_url().'Customers/login'?>"><span class="edu-icon edu-locked author-log-ic"></span>Log Out</a>
                                                         </li>
                                                     </ul>
                                                 </li>
@@ -731,8 +734,8 @@
                 </div>
                
             </div>
-             <br>
-        
+            </div> 
+         </div>
             <!-- Mobile Menu end -->
         
          

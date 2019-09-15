2120b632023987d03c5e8215b5608359279bda26
snehalk2210 <55176210+snehalk2210@users.noreply.github.com>
2019-09-15 11:52:31 +0530

    Add files via upload

---
+++ welcome_msg.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="<?php echo base_url().'assets/css/bootstrap.min.css'?>">
+	<link rel="stylesheet" href="<?php echo base_url().'assets/js/bootstrap.min.js'?>">
+</head>
+<body>
+<div>
+	<div class="contacts-area mg-b-15">
+            <div class="container-fluid">
+                <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                        
+                        
+                        
+                    </div>
+                    <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+                        
+                        <h1>Welcome to Organic Vegies!!!!!</h1>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+                    
+</body>
+</html>

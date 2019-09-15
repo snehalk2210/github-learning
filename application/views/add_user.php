c1483ed46986f6ed3fa4893976f5546154bbaa15
snehalk2210 <55176210+snehalk2210@users.noreply.github.com>
2019-09-15 11:50:14 +0530

    Add files via upload

--- application/views/add_user.php
+++ application/views/add_user.php
@@ -1,113 +1,72 @@
- <div class="basic-form-area mg-b-15">
-            <div class="container-fluid">
-                <div class="row"> 
+<?php include('includes/header2.php');?>
+<html>
+<head>
+	<link rel="stylesheet" href="<?php echo base_url().'assets/css/bootstrap.min.css'?>">
+	<link rel="stylesheet" href="<?php echo base_url().'assets/js/bootstrap.min.js'?>">
+</head>
  
-<div class="col-lg-7 col-md-7 col-sm-7 col-xs-12">
-                        <div class="sparkline9-list responsive-mg-b-30">
-                            <div class="sparkline9-hd">
-                                <div class="main-sparkline9-hd">
-                                    <h1>User Login </h1>
-                                </div>
-                            </div>
-                            <div class="sparkline9-graph">
-                                <div class="basic-login-form-ad">
-                                    <div class="row">
-                                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                            <div class="login-social-inner">
-                                                <a href="#" class="button btn-social basic-ele-mg-b-10 facebook span-left"> <span><i class="fa fa-facebook"></i></span> Facebook </a>
-                                                <a href="#" class="button btn-social basic-ele-mg-b-10 twitter span-left"> <span><i class="fa fa-twitter"></i></span> Twitter </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                            <div class="basic-login-inner">
-                                                <h3>Sign In</h3>
-                                                <p> Register New User sign here</p>
-                                               <!--<form method="post" name="frm" id="frm" action="<?php echo base_url().'User/add_user_details'?>">-->
-                                               <?php echo form_open('User/add_user_details');?>
-                                                <div class="form-group-inner">
-                                                        <div class="row">
-                                                            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                                                                <label class="login2">User Name</label>
-                                                            </div>
-                                                            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-                                                                <input type="text" name="usr_name" id="usr_name" class="form-control" placeholder="Enter Your Name" />
-                                                            </div>
-                                                            <div>
-                                                            	<?php echo form_error('usr_name');?>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="form-group-inner">
-                                                        <div class="row">
-                                                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                                                                <label class="login2 pull-right pull-right-pro">User Type</label>
-                                                            </div>
-                                                            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-                                                                <div class="form-select-list">
-                                                                    <select class="form-control custom-select-value" name="usr_type" id="usr_type">
-																			<option value="">--Select your user type</option>
-				<?php 
-				for($i=0;$i<count($types);$i++){?>								<option value="<?php echo $types[$i]['user_type_id'];?>"><?php echo $types[$i]['user_type'];?></option>
-																			
-				<?php }?>														</select>
-                                                                </div>
-                                                            </div>
-                                                            <div>
-                                                            	
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="form-group-inner">
-                                                        <div class="row">
-                                                            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                                                                <label class="login2">Email</label>
-                                                            </div>
-                                                            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-                                                                <input type="email" name="email" id="email" class="form-control" placeholder="Enter Email" />
-                                                            </div>
-                                                            
-                                                        </div>
-                                                    </div>
-                                                    <div class="form-group-inner">
-                                                        <div class="row">
-                                                            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                                                                <label class="login2">Password</label>
-                                                            </div>
-                                                            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-                                                                <input type="password" name="pwd" id="pwd" class="form-control" placeholder="password" />
-                                                            </div>
-                                                            
-                                                        </div>
-                                                    </div>
-                                                    <div class="login-btn-inner">
-                                                        <div class="row">
-                                                            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12"></div>
-                                                            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-                                                                <div class="i-checks">
-                                                                    <label>
-																			<input type="checkbox"><i></i> Remember me </label>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                        <div class="row">
-                                                            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12"></div>
-                                                            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-                                                                <div class="login-horizental">
-                                                                    <button class="btn btn-sm btn-success login-submit-cs" type="submit" >Sign In</button>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-              </div>
-             </div> 
+ 
+ <div class="container" style="margin-top:20px;">
+<h1>User Login</h1>
+
+<?php if($error=$this->session->flashdata('Login_failed')):?>
+
+<div class="row">
+<div class="col-lg-6">
+<div class="alert alert-danger">
+<?php echo $error;?>
+	
+</div>
+	
+</div>
+	
+</div>
+
+<?php endif; ?>
+
+
+<!--<form action="base_url('admin/index')">-->
+<?php echo form_open('User/user_login');?>
+    <div class="row">
+    <div class="col-lg-6" >
+    <div class="form-group">
+    	<label for="Username">Username</label>
+    	
+    	<!--<input type="text" class="form-control" id="usr_name" placeholder="Enter Username" name="usr_name" value="set_value">-->
+    	
+    	<?php 
+    	$data=array('class'=>'form-control','placeholder'=>'Enter Username','name'=>'usr_name','id'=>'usr_name','value'=>set_value("usr_name"));
+    	echo form_input($data);?>
+    	
+    </div>
+    </div>
+    <div class="col-lg-6" style="margin-top:20px;">
+        <?php echo form_error('usr_name');?>
+    </div>
+    </div>
+    <div class="row">
+    <div class="col-lg-6">
+    <div class="form-group">
+    	<label for="pwd">Password</label>
+    	
+    	<input type="password" class="form-control" id="pwd" placeholder="Enter Password" name="pwd" >
+    	<!--<?php 
+    	$pass=array('class'=>'form-control','id'=>'pwd','name'=>'pwd','placeholder'=>'Enter Password','value'=>set_value("pwd"));
+    	echo form_input($pass);?>-->
+    </div>
+    </div >
+    <div class="col-lg-6" style="margin-top:20px;">
+    	<?php echo form_error('pwd');?>
+    </div>
+        
+    </div>
+    
+     <button type="submit" class="btn btn-default">Submit</button>
+	 <button type="reset" class="btn btn-default">Reset</button>
+	 <!--<?php $submit=array('type'=>'submit',
+	               'class'=>'btn btn-default',
+	               'value'=>'submit');?>
+	 <?php echo form_submit($submit);?>-->
+	 <a href="register/" class="link-class">Sign up?</a>
+	 
+</div>

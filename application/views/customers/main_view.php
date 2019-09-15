bb4a6a2fe426d479d02c60a4e1597f9be09e8b04
snehalk2210 <55176210+snehalk2210@users.noreply.github.com>
2019-09-15 10:04:46 +0530

    Add files via upload
    main view file

---
+++ application/views/customers/main_view.php
@@ -0,0 +1,7 @@
+<?php include('includes/header2.php');?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="<?php echo base_url().'assets/css/bootstrap.min.css'?>">
+	</head>
+</html>
